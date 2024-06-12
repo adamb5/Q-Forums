183bc75a7892e2c84af83e124b942486edc10f34
Adam <pollakaj@dukes.jmu.edu>
2024-06-12 14:47:14 -0400

    test update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import './App.css';
-import SearchBar from './components/SearchBar';
-import Navbar from './components/NavBar';
+import React from "react";
+import "./App.css";
+import SearchBar from "./components/SearchBar";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <h1 className="title">
-        Q-Forums
-      </h1>
+      <Navbar />
+      <h1 className="title">Q-Forums 2</h1>
       <SearchBar />
     </div>
   );
 }
 
-export default App; 
+export default App;
