174421e5f4ccbdde3af74e50b2d47ba96ac4fee9
arpan2004 <arpan2004.AD@gmail.com>
2024-06-27 09:25:48 -0400

    stack controller

--- backend/stack-controller.js
+++ backend/stack-controller.js
@@ -1,29 +1,56 @@
 const axios = require("axios");
 
-
-
 const getFormResults = (req, res, next) => {
-    //const tagged = req.params.tagged;
-    console.log("tagged: " + tagged);
-    res.send(`<form action="/stack/getans" method="GET"><input type="text" name="keyword"> <button type="submit"> Enter Search </button></form>`);
+  //const tagged = req.params.tagged;
+  console.log("tagged: " + tagged);
+  res.send(
+    `<form action="/stack/getans" method="GET"><input type="text" name="keyword"> <button type="submit"> Enter Search </button></form>`
+  );
 };
 
+let completeData = [];
 
-const getResult = (request, response, next) => {
-    const tagged = request.params.tagged;
-    console.log("tagged: " + tagged);
-    axios
-        .get(`https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&tagged=${tagged}&site=stackoverflow`)
-        .then((res) => {
-            //response.send('<h1> User' + '</h1>');
-            response.json(res.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    };
-    
-
+const getResult = async(request, response, next) => {
+  const tagged = request.params.tagged;
+  console.log("tagged: " + tagged);
+  try {
+    for (let i = 0; i < 150; i++) {
+      const response = await axios.get(
+        `https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&tagged=${tagged}&site=stackoverflow`
+      );
+      const newData = response.data;
+      newData.items.forEach((item) => {
+        completeData.push(item);
+      });
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
 
 exports.getFormResults = getFormResults;
 exports.getResult = getResult;
+
+// async function fetchData() {
+//   try {
+//     for (let i = 0; i < 150; i++) {
+//       const response = await axios.get(
+//         "https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&taggedsite=stackoverflow"
+//       );
+//       const newData = response.data;
+//       newData.items.forEach((item) => {
+//         completeData.push(item);
+//       });
+//     }
+//     // Write completeData to a file (optional)
+//     fs.writeFileSync(
+//       "stackoverflow_data.json",
+//       JSON.stringify(completeData, null, 2)
+//     );
+//     console.log("Data fetched and saved successfully.");
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//   }
+// }
+
+// fetchData();
