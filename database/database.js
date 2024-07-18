758a44a639fd1980782155aae5c0234c9094fd87
Adam Pollak <pollakaj@dukes.jmu.edu>
2024-07-18 10:51:34 -0400

    teswt

--- database/database.js
+++ database/database.js
@@ -7,7 +7,7 @@ try {
     .createPool({
       host: process.env.DATABASE_HOST,
       port: "3306",
-      user: process.env.DATABASE_USER,
+      user: "admin",
       password: process.env.DATABASE_PASSWORD,
       database: "q-forums-db",
       multipleStatements: true,
