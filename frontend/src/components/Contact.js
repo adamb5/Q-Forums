9ca503cf10858405759106c0da819ea87b8c81c1
Adam Pollak <pollakaj@dukes.jmu.edu>
2024-07-25 15:10:57 -0400

    style

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
   const contactStyle = {
-    backgroundColor: '#020f2fe3', // dark blue
-    color: 'white',
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-
+    backgroundColor: "#020f2fe3", // dark blue
+    color: "white",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   };
 
   return (
     <div style={contactStyle}>
       <h1>QinetiQ US IC-IA Business Unit</h1>
-      <p> Adam Brook: adam.brook@us.qinetiq.com, Arpan Das: arpan.das@us.qinetiq.com, Trisha Nittala: trisha.nittala@us.qinetiq.com </p>
-      <p> Samriddhi Kumar: samriddhi.kumar@us.qinetiq.com, Adam Pollak: adam.pollak@us.qinetiq.com </p>
+      <p>
+        {" "}
+        Adam Brook: adam.brook@us.qinetiq.com, Arpan Das:
+        arpan.das@us.qinetiq.com, Trisha Nittala: trisha.nittala@us.qinetiq.com{" "}
+      </p>
+      <p>
+        {" "}
+        Samriddhi Kumar: samriddhi.kumar@us.qinetiq.com, Adam Pollak:
+        adam.pollak@us.qinetiq.com{" "}
+      </p>
     </div>
   );
 };
