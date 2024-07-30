d4ce0b72d12882708b424e14aa30eafe2ed439fc
Adam Pollak <pollakaj@dukes.jmu.edu>
2024-07-30 11:48:12 -0400

    PLEASE 2

--- backend/app.js
+++ backend/app.js
@@ -185,8 +185,7 @@ app.post("/api/search", async (req, res) => {
     const body = post.cve.descriptions[0].value;
     const suspicious = 0; //false is 0
     //const label = "vulnerability";
-    const text = title.concat(body);
-    const label = await getPrediction(text);
+    const label = await getPrediction(body);
     console.log(label);
 
     // const {tag} = tagged;
